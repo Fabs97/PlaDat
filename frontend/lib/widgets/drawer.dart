fa4994030a426c4444d4c104252cee7c86f00a96
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2020-11-06 12:46:45 +0100

    [Frontend] - Basic appbar and drawer

---
+++ frontend/lib/widgets/drawer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer {
+  static Drawer createDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Text("Welcome to PlaDat"),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text("Create placement"),
+            onTap: () => Navigator.pushNamed(context, "/new-placement"),
+          ),
+        ],
+      ),
+    );
+  }
+}
