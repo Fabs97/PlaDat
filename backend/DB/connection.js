8964f204b558f2d60b24d5058cf0f3fbfb17fef3
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2020-11-10 09:15:48 +0100

    [Backend] - Staging and production db configs, extended README

--- backend/DB/connection.js
+++ backend/DB/connection.js
@@ -1,6 +1,7 @@
-const environment = 'development';
-const config = require('../knexfile');
-const environmentConfig = config[environment];
+// const environment = 'development';
+// const config = require('../knexfile');
+// const environmentConfig = config[environment];
+const environmentConfig = require('../knexfile');
 const knex = require('knex');
 const connection = knex(environmentConfig);
 
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,31 +1,29 @@
 // Update with your config settings.
-
 require('dotenv').config({ path: './.env' })
 
-module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: ()=> ({
+let DBConnection;
+switch (process.env.NODE_ENV) {
+  case "staging":
+  case "production":
+    DBConnection = process.env.DATABASE_URL;
+    break;
+      
+  case "development":
+    DBConnection = ({
       host: process.env.DEV_DB_HOST,
       database: process.env.DEV_DB_NAME,
       user: process.env.DEV_DB_USR,
       port: process.env.DEV_DB_PORT,
       password: process.env.DEV_DB_PWD
-    }),
-    cwd: "./DB/migrations",
-    migrations: {
-      directory: './DB/migrations'
-    }
-  },
+    });
+    break;
+}
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   }
-  // }
-
+module.exports = {
+  client: 'pg',
+  connection: DBConnection,
+  cwd: "./DB/migrations",
+  migrations: {
+    directory: './DB/migrations'
+  }
 };
