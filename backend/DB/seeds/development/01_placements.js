3a32a603f572f65d3035fd7a0e53d7a439efa4db
AidaO <aida.denisa@gmail.com>
2020-11-23 12:51:04 +0000

    [BACKEND] Changed the seeds insertion for placement to take the ids of the companies from DB

--- backend/DB/seeds/development/01_placements.js
+++ backend/DB/seeds/development/01_placements.js
@@ -2,19 +2,22 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('placements').del()
-    .then(function () {
+    .then(async function () {
+      let employers = await knex('employer')
+        .select('id', 'name')
+        .whereIn('name', ['Google', 'Amazon', 'Instagram', 'Pinterest', 'Facebook', 'Reply', 'Oracle'])
       // Inserts seed entries
       return knex('placements').insert([
-        {position: 'Frontend Developer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 1},
-        {position: 'Backend Developer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 1},
-        {position: 'Fullstack Developer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 1},
-        {position: 'Project Manager', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 2},
-        {position: 'Quality Manager', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 2},
-        {position: 'DevOps', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 3},
-        {position: 'Designer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 4},
-        {position: 'Accountant', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 4},
-        {position: 'Business Analyst', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 5},
-        {position: 'IT Project Manager', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: 6}
+        {position: 'Frontend Developer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[0].id},
+        {position: 'Backend Developer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[0].id},
+        {position: 'Fullstack Developer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[0].id},
+        {position: 'Project Manager', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[1].id},
+        {position: 'Quality Manager', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[1].id},
+        {position: 'DevOps', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[2].id},
+        {position: 'Designer', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[2].id},
+        {position: 'Accountant', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[3].id},
+        {position: 'Business Analyst', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[4].id},
+        {position: 'IT Project Manager', working_hours: "25", start_period: "2021/01/30", end_period: "2021/06/30", salary: "800", description_role: "Interesting role to improve personal skills", employer_id: employers[5].id}
       ]);
     });
 };
