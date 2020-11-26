0e9ba161a4c529d0b8d12c828e2ac73d7c9fee53
Alice <alicecasali96@gmail.com>
2020-11-26 18:06:02 +0100

    [FRONTEND] - Updated placement part match view

--- frontend/lib/widgets/match_alert.dart
+++ frontend/lib/widgets/match_alert.dart
@@ -13,6 +13,7 @@ class MatchAlert extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     final bool isStudent = object is Student;
     return Scaffold(
       backgroundColor: Colors.grey[850],
@@ -30,74 +31,104 @@ class MatchAlert extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Congratulations!',
-              maxLines: 20,
-              style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            Text(
-              'You have a match.',
-              maxLines: 20,
-              style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
+            Container(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Congratulations!',
+                    maxLines: 20,
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    'You have a match.',
+                    maxLines: 20,
+                    style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
             ),
             isStudent
-                ? Column(
-                    children: [
-                      Text(
-                        object.name + ' ' + object.surname,
-                        maxLines: 22,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        'School of Life',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  )
-                : Center(
+                ? Container(
+                    padding: const EdgeInsets.all(40.0),
                     child: Column(
                       children: [
                         Text(
-                          'Google, Zurich',
-                          maxLines: 20,
+                          object.name + ' ' + object.surname,
+                          maxLines: 22,
                           style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
+                        Text(
+                          'School of Life',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        )
                       ],
                     ),
+                  )
+                : Column(
+                    children: [
+                      Container(
+                        // Image container
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: ExactAssetImage("/images/image0.jpg"),
+                              fit: BoxFit.fill,
+                            ),
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0)),
+                        width: size.width * .2,
+                        height: size.width * .2,
+                      ),
+                      Text(
+                        'Google, Zurich',
+                        maxLines: 20,
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
                   ),
-            Text(
-              'for',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              placement.position,
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            Container(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                children: [
+                  Text(
+                    'for',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    placement.position,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
