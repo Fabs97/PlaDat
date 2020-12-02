8faf71e2c14030d189e4e3324a3caa03b8067af6
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2020-12-02 11:14:08 +0100

    [Migrations] - Changed enum to constant values all caps

--- backend/DB/migrations/20201202101549_placement_hours_to_type.js
+++ backend/DB/migrations/20201202101549_placement_hours_to_type.js
@@ -3,7 +3,7 @@ exports.up = async function(knex) {
     let hasTable = await knex.schema.hasTable("placements");
     return hasTable ? knex.schema.table('placements', (table) => {
         table.dropColumn('working_hours');
-        table.enum("employment_type", ["Full Time", "Part Time", "Contract", "Internship"]);
+        table.enum("employment_type", ["FULL_TIME", "PART_TIME", "CONTRACT", "INTERNSHIP"]);
     }) : null;
 };
 
