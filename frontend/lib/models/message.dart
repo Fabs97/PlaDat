2ba723c38d69ec0deb531ba4d9c738f07ca928bd
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2020-12-03 00:01:23 +0100

    [Models] - Messages model to be compliant with backend

--- frontend/lib/models/message.dart
+++ frontend/lib/models/message.dart
@@ -1,20 +1,56 @@
 import 'dart:convert';
 
+enum Sender { STUDENT, EMPLOYER }
+
+extension SenderExtension on Sender {
+  String get string {
+    switch (this) {
+      case Sender.EMPLOYER:
+        return "EMPLOYER";
+      case Sender.STUDENT:
+        return "STUDENT";
+    }
+  }
+
+  static Sender getFrom(String stringType) {
+    switch (stringType) {
+      case "STUDENT":
+        return Sender.STUDENT;
+      case "EMPLOYER":
+        return Sender.EMPLOYER;
+      default:
+        return null;
+    }
+  }
+}
+
 class Message {
   final int id;
-  final int addresser;
-  final int addressee;
+  final int studentId;
+  final int employerId;
   final String message;
+  final DateTime sendDate;
+  final Sender sender;
 
-  Message({this.id, this.addresser, this.addressee, this.message});
+  Message(
+      {this.sender,
+      this.id,
+      this.studentId,
+      this.employerId,
+      this.message,
+      this.sendDate});
 
   factory Message.fromJson(Map<String, dynamic> json) {
     if (json == null) return null;
     return Message(
       id: json["id"] as int,
-      addresser: json["addresser"] as int,
-      addressee: json["addressee"] as int,
+      studentId: json["student_id"] as int,
+      employerId: json["employer_id"] as int,
       message: json["message"] as String,
+      sendDate: json["send_date"] != null
+          ? DateTime.parse(json["send_date"])
+          : null,
+      sender: SenderExtension.getFrom(json["sender"]),
     );
   }
 
@@ -25,9 +61,11 @@ class Message {
   Map<String, dynamic> toJsonMap() {
     return {
       "id": this.id,
-      "addresser": this.addresser,
-      "addressee": this.addressee,
+      "studentId": this.studentId,
+      "employerId": this.employerId,
       "message": this.message,
+      "sendDate": this.sendDate.toString(),
+      "sender": this.sender.string,
     };
   }
 }
