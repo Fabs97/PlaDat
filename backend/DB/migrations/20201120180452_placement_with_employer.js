0a2dfcaf5483a15c725c295cd1603255f7ba70d2
AidaO <aida.denisa@gmail.com>
2020-12-04 17:29:36 +0000

    Added seeds for degrees and fixed some issues with some migrations

--- backend/DB/migrations/20201120180452_placement_with_employer.js
+++ backend/DB/migrations/20201120180452_placement_with_employer.js
@@ -1,6 +1,7 @@
 exports.up = async function(knex) {
-    let hasTable = await knex.schema.hasTable('placements')
-    return hasTable ? knex.schema.table('placements', (table)=>{
+    let hasTable = await knex.schema.hasTable('placements');
+    let check = await knex.schema.hasColumn('placements','employer_id');
+    return (hasTable && !check) ? knex.schema.table('placements', (table)=>{
         table.integer('employer_id').unsigned().references('id').inTable('employer').notNullable();
     }) : null;
 };
