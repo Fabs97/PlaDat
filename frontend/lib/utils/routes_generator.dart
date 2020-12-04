3cd93d5e82c3d439760195b5da07cecfb55264fd
Alice <alicecasali96@gmail.com>
2020-12-04 17:37:37 +0100

    [FRONTEND] - Created new navigation to show employer placements

--- frontend/lib/utils/routes_generator.dart
+++ frontend/lib/utils/routes_generator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/main.dart';
+import 'package:frontend/screens/company_placement_list/company_placement_list.dart';
 import 'package:frontend/screens/company_student_list/company_student_list.dart';
 import 'package:frontend/screens/new_placement/new_placement.dart';
 import 'package:frontend/screens/new_student/new_student.dart';
@@ -28,6 +29,10 @@ class RoutesGenerator {
       case '/new-student':
         {
           return _createRoute(NewStudent(), settings);
+        }
+      case '/company-placements':
+        {
+          return _createRoute(MyPlacements(), settings);
         }
       default:
         {
