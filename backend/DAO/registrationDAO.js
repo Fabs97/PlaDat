edbc9195d292c1ada47b9b595dc7e2d3ce840ea8
Alice <alicecasali96@gmail.com>
2020-12-06 11:57:22 +0100

    [BACKEND] - Handling of 500 error

--- backend/DAO/registrationDAO.js
+++ backend/DAO/registrationDAO.js
@@ -1,5 +1,9 @@
 const database = require('../DB/connection');
 const connection = require('../DB/connection');
+const SuperError = require('../errors').SuperError;
+const ERR_INTERNAL_SERVER_ERROR = require('../errors').ERR_INTERNAL_SERVER_ERROR;
+
+
 
 module.exports = {
     
@@ -8,7 +12,12 @@ module.exports = {
             email: userInfo.email,
             password: userInfo.password,
             type: userInfo.type
-        },['id', 'email', 'type']);
+        },['id', 'email', 'type'])
+        .catch(error => {
+            if(error){
+                throw new SuperError(ERR_INTERNAL_SERVER_ERROR, 'There has been a problem saving your registration. Please try again')
+            }
+        });
 
         return result[0];
     },
--- backend/routes/registrationRoute.js
+++ backend/routes/registrationRoute.js
@@ -3,7 +3,10 @@ const router = require('express').Router();
 const registrationService = require('../services/registrationService');
 
 router.post("/registration", async ( req, res, next ) => {
-    let userRegistration = await registrationService.createUserRegistration(req.body);
+    let userRegistration = await registrationService.createUserRegistration(req.body)
+        .catch(error => {
+            res.status(error.code).send(error.message);
+        });
     res.json(userRegistration);
 });
 
