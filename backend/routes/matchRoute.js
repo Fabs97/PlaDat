01b9288d324d2d9735093e00734baf898d2c8319
Alice <alicecasali96@gmail.com>
2020-12-13 18:40:59 +0100

    [BACKEND] - Delete match API

--- backend/routes/matchRoute.js
+++ backend/routes/matchRoute.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const matchService = require('../services/matchService');
+const SuperError = require('../errors').SuperError;
+const ERR_BAD_REQUEST = require('../errors').ERR_BAD_REQUEST;
+
 
 router.post("/matching", async (req, res, next) => {
     const choice = await matchService.saveChoice(req.body);
@@ -14,6 +17,19 @@ router.get('/student/:studentId/placements', async (req, res, next) => {
             res.status(error.code).send(error.message);
         });
     res.json(placements);
+});
+
+router.delete('/match/:studentId/:placementId', async (req, res, next) => {
+    if( !isNaN(req.params.studentId) && !isNaN(req.params.placementId)){
+        let match = await matchService.deleteMatch(req.params.studentId, req.params.placementId)
+        .catch(error => {
+            res.status(error.code).send(error.message);
+        });
+        res.json(match);
+    } else {
+        res.status(ERR_BAD_REQUEST).send('Your request did not provided valid values for ids. Please try again.');
+    }
+    
 });
 
 module.exports = router;
