0388e89c5147a77eeeeb1cd4c2028d504210ba51
AnnaBergamasco <anna.bergamasco@mail.polimi.it>
2020-12-14 00:01:23 +0100

    [backend] - changes to google maps api

--- backend/routes/googleMapsRoute.js
+++ backend/routes/googleMapsRoute.js
@@ -1,12 +1,17 @@
 
 const router = require('express').Router();
 
-const googleMapsService = require('../services/googleMapsService');
+const https = require('https');
+require('dotenv').config({ path: '../.env' })
+
 
 router.get("/googleMaps", async (req, res, next) => {
-    let result = await googleMapsService.getMapResult(req.body.input);
-    res.send(result)
-
+    
+    let connector = https.get("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=\"" + req.body.input + "\"&types=address&language=en&key=" + process.env.GPLACES_APIKEY , (resp) => {
+        resp.pipe(res);
+        res.setHeader('content-type', 'application/json');
+      })
+    req.pipe(connector);
 })
 
 module.exports = router;
