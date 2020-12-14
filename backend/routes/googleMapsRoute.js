8e2736a587bd475ab82a87d0c1d58225d664d57c
AnnaBergamasco <anna.bergamasco@mail.polimi.it>
2020-12-14 19:38:23 +0100

    [backend] - fixes to goolgle maps api

--- backend/routes/googleMapsRoute.js
+++ backend/routes/googleMapsRoute.js
@@ -5,9 +5,9 @@ const https = require('https');
 require('dotenv').config({ path: '../.env' })
 
 
-router.get("/googleMaps", async (req, res, next) => {
+router.get("/googleMaps/:input", async (req, res, next) => {
     
-    let connector = https.get("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=\"" + req.body.input + "\"&types=address&language=en&key=" + process.env.GPLACES_APIKEY , (resp) => {
+    let connector = https.get("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=\"" + req.params.input + "\"&types=address&language=en&key=" + process.env.GPLACES_APIKEY , (resp) => {
         resp.pipe(res);
         res.setHeader('content-type', 'application/json');
       })
