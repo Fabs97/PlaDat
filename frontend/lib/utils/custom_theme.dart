709c7733fa0382237c08dcb22de15efb7533fe25
Alice <alicecasali96@gmail.com>
2020-12-15 18:06:16 +0100

    [FRONTEND] - General restyling from CustomTheme

--- frontend/lib/utils/custom_theme.dart
+++ frontend/lib/utils/custom_theme.dart
@@ -5,8 +5,16 @@ class CustomTheme {
   final _backgroundColor = Color(0xfff2f6ff);
   final _primaryColor = Color(0xff4c60d2);
   final _secondaryColor = Color(0xfff48565);
-  final _accentColor = Color(0xffffd9cf);
   final _textColor = Color(0xff4c4c4c);
+  final _accentColor = Color(0xffffd9cf);
+  final _accentTextColor = Color(0xffe23300);
+
+  Color get backgroundColor => this._backgroundColor;
+  Color get primaryColor => this._primaryColor;
+  Color get secondaryColor => this._secondaryColor;
+  Color get textColor => this._textColor;
+  Color get accentColor => this._accentColor;
+  Color get accentTextColor => this._accentTextColor;
 
   ThemeData get appThemeData => ThemeData(
         primaryColor: _backgroundColor,
@@ -33,6 +41,7 @@ class CustomTheme {
           deleteIconColor: Colors.white,
           labelStyle: TextStyle(color: Colors.white),
         ),
+        primaryIconTheme: IconThemeData(color: _primaryColor),
         scaffoldBackgroundColor: _backgroundColor,
         errorColor: _secondaryColor,
         // toggleableActiveColor: _secondaryColor,
@@ -40,6 +49,14 @@ class CustomTheme {
           selectedColor: _secondaryColor,
           disabledColor: Color(0xff7e7e7e),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: TextButton.styleFrom(primary: Colors.white)),
         buttonColor: _primaryColor,
+        cardTheme: CardTheme(
+          shadowColor: Color(0xffced5ff),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              side: BorderSide(color: Color(0xffced5ff), width: 8.0)),
+        ),
       );
 }
