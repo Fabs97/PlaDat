884a3ae4c4cd8a78628b26fd1a1d26aa8d3ba8aa
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2021-01-10 18:47:49 +0100

    [Backend] - documentation root path

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ var options = {
       authAction :{ JWT: {name: "JWT", schema: {type: "apiKey", in: "header", name: "Authorization", description: ""}, value: "Bearer <JWT>"} }
     }
   };
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(APIDocumentation, options));
+app.use('/', swaggerUI.serve, swaggerUI.setup(APIDocumentation, options));
 
 app.use(express.json());
 
@@ -48,8 +48,6 @@ app.use('/', authMiddleware, educationRoute);
 app.use('/', authMiddleware, messageRoute);
 app.use('/', authMiddleware, domainOfActivityRoute);
 
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`PlaDat backend listening on port ${port}!`));
 
 module.exports = app;
