378068f266a5345432c924b14cc9d0357e2384fe
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2021-01-13 16:28:54 +0100

    [Frontend] - Changed web title and description

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'PlaDat - Placement Dating for Students',
       theme: CustomTheme().appThemeData,
       navigatorKey: Nav.navigatorKey,
       initialRoute: "/login",
